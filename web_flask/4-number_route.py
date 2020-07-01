e4143dd3c92964e23465c95667e6e9a13c21b25b
Naouali <elnaoualinebil@gmail.com>
2020-07-01 16:36:40 +0100

    is integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+module to create a page/route
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_route():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb_route():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_route(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>')
+def number_route(n):
+    if isinstance(n) is int:
+        return n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
